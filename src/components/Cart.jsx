b3c603b005f10b789b1288a5bf2e514d7e038c84
codewithswatiee <verma.s@somaiya.edu>
2024-07-27 11:05:04 +0530

    half way there

---
+++ src/components/Cart.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react'
+import Modal from './UI/Modal'
+import CartContext from '../store/CartContext'
+import { currencyFormatter } from '../util/formatting';
+import Button from './UI/Button';
+import UserProgressContext from '../store/UserProgressContext';
+import CartItem from './CartItem';
+
+function Cart() {
+    const cartCtx = useContext(CartContext);
+    const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price, 0);
+    const userProgeressCtx = useContext(UserProgressContext);
+
+    function handleCloseCart() {
+        userProgeressCtx.hideCart();
+    }
+  return (
+    <Modal className='cart' open={userProgeressCtx.progress === 'cart'}>
+        <h2>Your Cart</h2>
+        <ul>
+            {cartCtx.items.map(item => 
+                <CartItem 
+                    key={item.id} 
+                    item={item} 
+                    onIncrease={() => cartCtx.addItem(item)} 
+                    onDecrease={() => cartCtx.removeItem(item.id)}>
+                </CartItem>)}
+        </ul>
+        <p className='cart-total'>{currencyFormatter.format(cartTotal)}</p>
+        <p className='modal-actions'>
+            <Button textOnly onClick={handleCloseCart}> Close </Button>
+            {cartCtx.items.length > 0 &&  <Button onClick={handleCloseCart}>Go to Checkout</Button>}
+        </p>
+    </Modal>
+  )
+}
+
+export default Cart
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,20 @@ import React, { useContext } from 'react'
 import logoImg from '../assets/logo.jpg'
 import Button from './UI/Button';
 import CartContext from '../store/CartContext';
+import UserProgressContext from '../store/UserProgressContext';
 function Header() {
+
+  const userProgeressCtx = useContext(UserProgressContext);
+
 
   const cartCtx = useContext(CartContext);
   const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
     return totalNumberOfItems + item.quantity;
   }, 0);
+
+  function handleShowCart(){
+    userProgeressCtx.showCart();
+  }
   
   return (
     <header id='main-header'>
@@ -16,7 +24,7 @@ function Header() {
             <h1>Food ordering App</h1>
         </div>
         <nav>
-            <Button textOnly> Cart ({totalCartItems}) </Button>
+            <Button textOnly onClick={handleShowCart}> Cart ({totalCartItems}) </Button>
         </nav>
     </header>
   )
